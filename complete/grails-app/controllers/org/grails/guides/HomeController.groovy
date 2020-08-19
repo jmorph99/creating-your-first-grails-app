ceb9408b85f4ee6924a9364dbe5a320c509d5025
John Murphy <john.murphy@elastic.co>
2020-08-19 13:23:53 -0400

    Search and aggregation

--- complete/grails-app/controllers/org/grails/guides/HomeController.groovy
+++ complete/grails-app/controllers/org/grails/guides/HomeController.groovy
@@ -1,20 +1,53 @@
 package org.grails.guides
+import grails.plugins.elasticsearch.ElasticSearchService
+import grails.plugins.elasticsearch.ElasticSearchResult
+import org.elasticsearch.search.aggregations.Aggregation
+import org.elasticsearch.search.aggregations.AggregationBuilders
+import org.elasticsearch.search.aggregations.bucket.terms.ParsedStringTerms
+import org.elasticsearch.search.aggregations.bucket.terms.ParsedTerms
 
+//import grails.plugins.elasticsearch.
 class HomeController {
 
     def index() {
         respond([name: session.name ?: 'User', vehicleList: Vehicle.list(), vehicleTotal: Vehicle.count()])
 
     }
-
+    ElasticSearchService elasticSearchService
+    AggregationBuilders aggregationBuilders
 
     def updateName(String name) {
         session.name = params.name
-
+        String sname = params.name
         flash.message = 'Name has been updated'
-        def res = elasticSearch.search("${params.query}")
+        ElasticSearchResult res = elasticSearchService.search(sname, [indices:'blogs'])
+        response.setHeader("Content-Type", "text/html")
+        response.outputStream << "<h3>Titles</h3>"
+        res.searchResults.each {
+            response.outputStream << it['title'].toString() + "<br/>"
+        }
+        ArrayList<Aggregation> aggs = []
+        aggs << AggregationBuilders.terms('Authors').field('author.keyword')
+        ElasticSearchResult res2 = elasticSearchService.search(
+                sname,
+                null as Closure,
+                {
 
-        redirect action: 'index'
+                    "names" {
+                        terms(field: 'author.keyword')
+                    }
+
+                },
+                [ indices: 'blogs', type: "_doc" ])
+                response.outputStream << "<br/><br/><h3>Authors Aggregation</h3>"
+
+                res2.getAggregations().get('names').buckets.each {
+                    println it.key.toString()
+                    response.outputStream << it.docCount.toString() + " " + it.key.toString() + "<br/>"
+                }
+
+
+
     }
 
 }
